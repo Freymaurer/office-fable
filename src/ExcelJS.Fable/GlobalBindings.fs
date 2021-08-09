ec4684cbabf10f64aae98264e31639e3263ccb4e
Kevin F <Freymaurer@gmx.de>
2021-08-09 11:50:09 +0200

    Add excel only projec :tada:

---
+++ src/ExcelJS.Fable/GlobalBindings.fs
@@ -0,0 +1,21 @@
+﻿namespace OfficeJS.Fable
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+module Office =
+
+    open OfficeJS.Fable
+
+    [<Global>]
+    let Office : Office.IExports = jsNative
+
+
+module Excel =
+
+    [<Global>]
+    //[<CompiledName("Office.Excel")>]
+    let Excel : Excel.IExports = jsNative
+
+    [<Global>]
+    let ExcelRangeLoadOptions : Excel.Interfaces.RangeLoadOptions = jsNative
