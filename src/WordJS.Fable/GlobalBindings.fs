d2cb294a3a4f1b50c9788155597b3bb0991e1ad2
Kevin F <Freymaurer@gmx.de>
2021-08-09 11:54:07 +0200

    Add word only project :tada:

---
+++ src/WordJS.Fable/GlobalBindings.fs
@@ -0,0 +1,20 @@
+﻿namespace OfficeJS.Fable
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+
+module Office =
+
+    open OfficeJS.Fable
+
+    [<Global>]
+    let Office : Office.IExports = jsNative
+
+module Word =
+
+    [<Global>]
+    let Word : Word.IExports = jsNative
+
+    [<Global>]
+    let WordRangeLoadOptions : Word.Interfaces.RangeLoadOptions = jsNative
